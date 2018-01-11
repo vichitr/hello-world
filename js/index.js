73b2c048fbcf6193e19020d4c7d0dfa9ef9de9ea
Vichitr Gandas <vichitrgandas@gmail.com>
2018-01-11 16:23:10 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
 	$('.materialboxed').materialbox();
+	$('#intro-details').height($('#mydetails').height());
+	if (screen.width > 600)
+	{
+		$('#grey-screen').height($('#intro-details').height() - 25);
+	}
 });
