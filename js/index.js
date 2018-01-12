1c041f0cd2c5bf34fe5645567ecf6e5841ffff59
Vichitr Gandas <vichitrgandas@gmail.com>
2018-01-13 03:25:17 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,10 +8,10 @@ $(document).ready(function(){
 	}
 });
 $(window).scroll(function() {
-	if(($('#whoiam').offset().top - $(window).scrollTop() <= 35) && ($('.get-in-touch').offset().top() - $(window).scrollTop() > 40)) {
-		$('#barcolor').removeClass('white-text').addClass('purple-text').addClass('text-accent-3');
+	if(($('#whoiam').offset().top - $(window).scrollTop() <= 35)) {
+		$('.barcolor').removeClass('white-text').addClass('purple-text').addClass('text-accent-3');
 	}
 	else {
-		$('#barcolor').removeClass('purple-text').removeClass('text-accent-3').addClass('white-text');
+		$('.barcolor').removeClass('purple-text').removeClass('text-accent-3').addClass('white-text');
 	}
 });
