9efdaf929c9afde4d3b7f4975eaeb3877634a28b
Vichitr Gandas <vichitrgandas@gmail.com>
2018-01-12 22:45:58 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$(".button-collapse").sideNav();
 	$('.materialboxed').materialbox();
-	$('#intro-details').height($('#mydetails').height());
+	$('#mydetails').height($('#intro-details').height());
 	if (screen.width > 600)
 	{
 		$('#grey-screen').height($('#intro-details').height() - 25);
