f2e80e3b70c9b13f1584f9bf0d1c26414ec8ad71
Vichitr Gandas <vichitrgandas@gmail.com>
2018-01-13 15:41:56 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 $(window).scroll(function() {
 	if(($('#whoiam').offset().top - $(window).scrollTop() <= 25)) {
 		$('.barcolor').removeClass('white-text').addClass('purple-text').addClass('text-accent-2');
-		if($('#ftr').offset().top() - $(window).scrollTop() > 0 ) {
+		if($('#ftr').offset().top() - $(window).scrollTop() < 0 ) {
 			$('.barcolor').removeClass('purple-text').removeClass('text-accent-2').addClass('white-text');
 		}
 	}
